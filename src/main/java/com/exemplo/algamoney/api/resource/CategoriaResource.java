bc367a3d018f5913a013f41a82a85af685fd00ae
Jonas Diego Rodrigues Martins <jonasdiegorodriguesmartins@gmail.com>
2019-11-07 19:07:49 -0300

    Add update pessoa in service class

--- src/main/java/com/exemplo/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/exemplo/algamoney/api/resource/CategoriaResource.java
@@ -10,11 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.exemplo.algamoney.api.event.RecursoCriadoEvent;
@@ -51,5 +53,11 @@ public class CategoriaResource implements Serializable {
 	public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
 		Categoria categoria = categoriaRepository.findByCodigo(codigo);
 		return categoria != null ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+		categoriaRepository.deleteById(codigo);
 	}
 }
--- src/main/java/com/exemplo/algamoney/api/resource/PessoaResource.java
+++ src/main/java/com/exemplo/algamoney/api/resource/PessoaResource.java
@@ -9,16 +9,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.exemplo.algamoney.api.event.RecursoCriadoEvent;
 import com.exemplo.algamoney.api.model.Pessoa;
 import com.exemplo.algamoney.api.repository.PessoaRepository;
+import com.exemplo.algamoney.api.service.PessoaService;
 
 @RestController
 @RequestMapping("/pessoas")
@@ -26,6 +30,9 @@ public class PessoaResource {
 
 	@Autowired
 	private PessoaRepository pessoaRepository;
+	
+	@Autowired
+	private PessoaService pessoaService;
 	
 	@Autowired
 	private ApplicationEventPublisher publisher;
@@ -49,5 +56,18 @@ public class PessoaResource {
 		Pessoa pessoa = pessoaRepository.findByCodigo(codigo);
 		
 		return pessoa != null ? ResponseEntity.ok(pessoa) : ResponseEntity.notFound().build();		
+	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	private void remover(@PathVariable Long codigo) {
+		pessoaRepository.deleteById(codigo);
+	}
+	
+	@PutMapping("/{codigo}")
+	public ResponseEntity<Pessoa> atualizar(@PathVariable Long codigo, @Valid @RequestBody Pessoa pessoa) {
+		Pessoa pessoaSalva = pessoaService.atualizar(codigo, pessoa);
+		
+		return ResponseEntity.ok(pessoaSalva);
 	}
 }
